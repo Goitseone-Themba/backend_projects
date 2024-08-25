073b0bc200d24b2feb4054fea2e8a037b53360c6
Goitseone-Themba <goitseonerozthemba@gmail.com>
2024-08-25 23:50:47 +0200

    implemented read, write and add Custy in rust

---
+++ Customer-List/rs_custy/target/debug/build/serde_json-7102b9f6a8a540d8/build_script_build-7102b9f6a8a540d8.d
@@ -0,0 +1,5 @@
+/home/goitseone/Projects/web/dev/master-backend/Customer-List/rs_custy/target/debug/build/serde_json-7102b9f6a8a540d8/build_script_build-7102b9f6a8a540d8: /home/goitseone/.cargo/registry/src/index.crates.io-6f17d22bba15001f/serde_json-1.0.127/build.rs
+
+/home/goitseone/Projects/web/dev/master-backend/Customer-List/rs_custy/target/debug/build/serde_json-7102b9f6a8a540d8/build_script_build-7102b9f6a8a540d8.d: /home/goitseone/.cargo/registry/src/index.crates.io-6f17d22bba15001f/serde_json-1.0.127/build.rs
+
+/home/goitseone/.cargo/registry/src/index.crates.io-6f17d22bba15001f/serde_json-1.0.127/build.rs:
---
+++ Customer-List/rs_custy/target/debug/rs_custy.d
@@ -0,0 +1,1 @@
+/home/goitseone/Projects/web/dev/master-backend/Customer-List/rs_custy/target/debug/rs_custy: /home/goitseone/Projects/web/dev/master-backend/Customer-List/rs_custy/src/main.rs

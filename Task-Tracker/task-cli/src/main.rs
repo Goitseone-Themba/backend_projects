9562ac743532c61dd9ab853b52a855ac0c5860c8
Goitseone-Themba <goitseonerozthemba@gmail.com>
2024-09-20 15:15:36 +0200

    implement add functionality

--- Task-Tracker/task-cli/src/main.rs
+++ Task-Tracker/task-cli/src/main.rs
@@ -1,31 +1,33 @@
 use serde::{Deserialize, Serialize};
 use std::fs::File;
 use std::io::{Read, Write};
+use std::time::SystemTime;
 
 #[derive(Serialize, Deserialize, Debug)]
+
 // data model for each task
 struct Task {
     id: u32,
     description: String,
     status: String,
-    created_at: String,
-    updated_at: String,
+    created_at: SystemTime,
+    updated_at: SystemTime,
 }
 
 // data model for the json file
-struct TaskList {
+struct TaskCli {
     json_filename: String,
 }
 
-impl TaskList {
+impl TaskCli {
     fn new() -> Self {
-        TaskList {
+        TaskCli {
             json_filename: String::from("task_list.json"),
         }
     }
 
     // read from the json file
-    fn read(&self) -> Vec<Task> {
+    fn read_json(&self) -> Vec<Task> {
         let file_result = File::open(&self.json_filename);
 
         match file_result {
@@ -39,18 +41,49 @@ impl TaskList {
     }
 
     //write to the json file
-    fn write(&self, tasks: &Vec<Task>) {
+    fn write_json(&self, tasks: &Vec<Task>) {
         let json_string = serde_json::to_string_pretty(&tasks).unwrap();
         let mut file = File::create(&self.json_filename).unwrap();
-        file.write_all(json_string.as_bytes()).unwrap(); 
+        file.write_all(json_string.as_bytes()).unwrap();
     }
 
-    // fn add(&self, description: String) {}
+    //adding a new task
+    fn add(&self, description: String) {
+        let mut tasks = self.read_json();
+        let new_task: Task = Task {
+            id: u32::try_from(tasks.len() + 1).unwrap(),
+            description,
+            status: String::from("to_do"),
+            created_at: SystemTime::now(),
+            updated_at: SystemTime::now(),
+        };
+        tasks.push(new_task);
+        self.write_json(&tasks);
+        println!("Tasks added successfully (ID: {})", tasks.len());
+    }
 }
 
 fn main() {
-    let task_list: TaskList = TaskList::new();
+    let task_cli: TaskCli = TaskCli::new();
 
-    println!("{}", task_list.json_filename);
+    let args: Vec<String> = std::env::args().collect();
 
+    if args.len() < 3 {
+        println!("usage: {} <command> <argument>", args[0]);
+        return;
+    }
+
+    let command = &args[1];
+    let argument = &args[2];
+
+    match command.as_str() {
+        "add" => {
+            if args.len() != 3 {
+                println!("add usage: {} add <your task>", &args[0]);
+                return;
+            }
+            task_cli.add(String::from(argument))
+        }
+        _ => println!("command: \"{}\" not found", command),
+    }
 }

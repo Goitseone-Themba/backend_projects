72bde103dd90de12608d4a9be8680657595369fd
Goitseone-Themba <goitseonerozthemba@gmail.com>
2024-10-09 13:12:33 +0200

    parse command

--- Github-User-Activity/github-activity/src/main.rs
+++ Github-User-Activity/github-activity/src/main.rs
@@ -1,3 +1,18 @@
+use std::env;
+
+    fn get_activity(username: &String) {
+        println!("fetching {}'s activity...",username); 
+    }
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() == 0 {
+        println!("Usage: github-activity <username>");
+        return;
+    }
+
+    let username: &String = &args[1];
+    
+    get_activity(username);
 }

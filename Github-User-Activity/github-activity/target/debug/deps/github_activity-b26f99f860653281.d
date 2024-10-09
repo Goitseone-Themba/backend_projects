09fd03a0ea55130f6ee5d291582a727bdcfa39c8
Goitseone-Themba <goitseonerozthemba@gmail.com>
2024-10-09 12:31:43 +0200

    init github user activity

---
+++ Github-User-Activity/github-activity/target/debug/deps/github_activity-b26f99f860653281.d
@@ -0,0 +1,5 @@
+/home/goitseone/Projects/web/dev/backend_projects/Github-User-Activity/github-activity/target/debug/deps/libgithub_activity-b26f99f860653281.rmeta: src/main.rs
+
+/home/goitseone/Projects/web/dev/backend_projects/Github-User-Activity/github-activity/target/debug/deps/github_activity-b26f99f860653281.d: src/main.rs
+
+src/main.rs:
